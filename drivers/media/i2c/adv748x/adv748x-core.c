026217830073c1f3453c6bc11b769cca91defdd5
Geert Uytterhoeven <geert+renesas@glider.be>
2017-11-28 08:01:24 -0500

    media: i2c: adv748x: Restore full DT paths in kernel messages
    As of_node_full_name() now returns only the basename, the endpoint
    information printed became useless:
    
        adv748x 4-0070: Endpoint endpoint on port 7
        adv748x 4-0070: Endpoint endpoint on port 8
        adv748x 4-0070: Endpoint endpoint on port 10
        adv748x 4-0070: Endpoint endpoint on port 11
    
    Restore the old behavior by using "%pOF" instead:
    
        adv748x 4-0070: Endpoint /soc/i2c@e66d8000/video-receiver@70/port@7/endpoint on port 7
        adv748x 4-0070: Endpoint /soc/i2c@e66d8000/video-receiver@70/port@8/endpoint on port 8
        adv748x 4-0070: Endpoint /soc/i2c@e66d8000/video-receiver@70/port@10/endpoint on port 10
        adv748x 4-0070: Endpoint /soc/i2c@e66d8000/video-receiver@70/port@11/endpoint on port 11
    
    Fixes: a7e4cfb0a7ca4773 ("of/fdt: only store the device node basename in full_name")
    
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Reviewed-by: Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>
    Acked-by: Kieran Bingham <kieran.bingham+renesas@ideasonboard.com>
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- drivers/media/i2c/adv748x/adv748x-core.c
+++ drivers/media/i2c/adv748x/adv748x-core.c
@@ -751,14 +751,12 @@ static int adv748x_parse_dt(struct adv748x_state *state)
 		of_graph_parse_endpoint(ep_np, &ep);
 
 		v4l2_fwnode_endpoint_parse(of_fwnode_handle(ep_np), &v4l2_ep);
-		adv_info(state, "Endpoint %s on port %d",
-				of_node_full_name(ep.local_node),
-				ep.port);
+		adv_info(state, "Endpoint %pOF on port %d", ep.local_node,
+			 ep.port);
 
 		if (ep.port >= ADV748X_PORT_MAX) {
-			adv_err(state, "Invalid endpoint %s on port %d",
-				of_node_full_name(ep.local_node),
-				ep.port);
+			adv_err(state, "Invalid endpoint %pOF on port %d",
+				ep.local_node, ep.port);
 
 			continue;
 		}
